47f21eb696f946dc4eccb16b476b38373d75c469
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2020-07-17 17:56:42 +0000

    pressao Umidade Temperatura

--- js/chartLoad.js
+++ js/chartLoad.js
@@ -1,92 +1,5 @@
 const PCD_NAME = "PCD-001"
-function RHTabsoluto() {
 
-    var DataTemperatura = [];
-    var DataUmidade = [];
-
-    var RHTabsoluto = new CanvasJS.Chart("RHTabsoluto", {
-        animationEnabled: false,
-        zoomEnabled: false,
-        backgroundColor: "#C0C0C0",
-        title: {
-
-            text: "PCD-Temperatura/Umidade",
-            //padding: 5
-        },
-        exportEnabled: true,
-        legend: {
-            fontSize: 15,
-            fontFamily: "tamoha",
-            horizontalAlign: "center", // left, center ,right 
-            verticalAlign: "bottom",  // top, center, bottom
-        },
-
-        axisY: {
-            //maximum: 90,
-            interval: 5,
-            title: "Temperatura/Umidade",
-            titleFontSize: 15,
-            includeZero: true
-            //includeZero: false
-        },
-
-        axisX: {
-            interval: 30,
-            title: "Hora LOCAL",
-            titleFontSize: 15,
-            intervalType: "minute",
-            valueFormatString: "DD/MMM/YY HH:mm",
-            labelAngle: -25,
-            labelMaxWidth: 100, // change label width accordingly
-            crosshair: { enabled: true },
-            gridDashType: "dot",
-            gridThickness: 1,
-            valueFormatString: "HH:mm"
-        },
-        data: [{
-            type: "spline",
-            showInLegend: true,
-            name: "Temperatura",
-            color: "rgba(255,0,0,1)",
-            yValueFormatString: "Temperatura 00.00°C",
-            xValueType: "dateTime",
-            dataPoints: DataTemperatura
-        },
-        {
-            //axisYType: "secondary",
-            type: "spline",
-            showInLegend: true,
-            name: "Umidade",
-            color: "rgba(0,0,255,1)",
-            yValueFormatString: "Umidade #,##%",
-            xValueType: "dateTime",
-            dataPoints: DataUmidade
-        }],
-    });
-
-    function addData(json) {
-        let data = json["PCD_data"][PCD_NAME];
-
-        for (var i = 0; i < data.length; i++) {
-            let localtimestamp = (data[i].timestamp) * 1000
-            let datatimeUTC = ((localtimestamp) + ((10800) * 1000))
-            let HMS = new Date(localtimestamp).toISOString().slice(0, 19).replace('T', ' ');
-            DataTemperatura.push({
-                x: datatimeUTC,
-                y: data[i].Temperatura, label: HMS
-            });
-            DataUmidade.push({
-                x: datatimeUTC,
-                y: data[i].Umidade, label: HMS
-
-            });
-        }
-        RHTabsoluto.render();
-    }
-    //addData(dadosTeste())
-    $.getJSON("/rhtdata", addData);
-
-}
 function RHTrelativo() {
 
     var DataTemperatura = [];
@@ -183,6 +96,176 @@ function RHTrelativo() {
     $.getJSON("/rhtdata", addData);
 
 }
+
+
+function Pressure() {
+    var DataPressure = [];
+
+    var Pressure = new CanvasJS.Chart("Pressure", {
+        animationEnabled: false,
+        zoomEnabled: false,
+        backgroundColor: "#C0C0C0",
+        title: {
+
+            text: "PCD-Pressao Barometrica",
+            //padding: 5
+        },
+        exportEnabled: true,
+        legend: {
+            fontSize: 15,
+            fontFamily: "tamoha",
+            horizontalAlign: "center", // left, center ,right 
+            verticalAlign: "bottom",  // top, center, bottom
+        },
+        axisY: {
+            title: "Pressao Barometrica",
+            interval: 2,
+            titleFontSize: 15,
+            //includeZero: true
+            includeZero: false
+        },
+        axisX: {
+            interval: 30,
+            title: "Hora LOCAL",
+            titleFontSize: 15,
+            intervalType: "minute",
+            valueFormatString: "DD/MMM/YY HH:mm",
+            labelAngle: -25,
+            labelMaxWidth: 100, // change label width accordingly
+            crosshair: { enabled: true },
+            gridDashType: "dot",
+            gridThickness: 1,
+            valueFormatString: "HH:mm"
+        },
+        data: [{
+            type: "spline",
+            showInLegend: true,
+            name: "Pressao",
+            color: "rgba(0,0,255,0.7)",
+            yValueFormatString: "Pressão 0.0Pa",
+            xValueType: "dateTime",
+            dataPoints: DataPressure
+        }]
+    });
+
+    function addData(json) {
+        let data = json["PCD_data"][PCD_NAME];
+
+        for (var i = 0; i < data.length; i++) {
+            let localtimestamp = (data[i].timestamp) * 1000
+            let datatimeUTC = ((localtimestamp) + ((10800) * 1000))
+            let HMS = new Date(localtimestamp).toISOString().slice(0, 19).replace('T', ' ');
+            if (data[i].Pressao != 0) {
+                DataPressure.push({
+                    x: datatimeUTC,
+                    y: (data[i].Pressao / 1), label: HMS
+
+                });
+            }
+        }
+        Pressure.render();
+    }
+    //addData(dadosTeste())
+    $.getJSON("/rhtdata", addData);
+}
+setInterval(function () {
+    RHTabsoluto()
+    RHTrelativo()
+    RHTUmidade()
+    Pressure()
+    RHTtemperatura()
+    Pressure()
+}, 60000)
+
+/* function RHTabsoluto() {
+
+    var DataTemperatura = [];
+    var DataUmidade = [];
+
+    var RHTabsoluto = new CanvasJS.Chart("RHTabsoluto", {
+        animationEnabled: false,
+        zoomEnabled: false,
+        backgroundColor: "#C0C0C0",
+        title: {
+
+            text: "PCD-Temperatura/Umidade",
+            //padding: 5
+        },
+        exportEnabled: true,
+        legend: {
+            fontSize: 15,
+            fontFamily: "tamoha",
+            horizontalAlign: "center", // left, center ,right
+            verticalAlign: "bottom",  // top, center, bottom
+        },
+
+        axisY: {
+            //maximum: 90,
+            interval: 5,
+            title: "Temperatura/Umidade",
+            titleFontSize: 15,
+            includeZero: true
+            //includeZero: false
+        },
+
+        axisX: {
+            interval: 30,
+            title: "Hora LOCAL",
+            titleFontSize: 15,
+            intervalType: "minute",
+            valueFormatString: "DD/MMM/YY HH:mm",
+            labelAngle: -25,
+            labelMaxWidth: 100, // change label width accordingly
+            crosshair: { enabled: true },
+            gridDashType: "dot",
+            gridThickness: 1,
+            valueFormatString: "HH:mm"
+        },
+        data: [{
+            type: "spline",
+            showInLegend: true,
+            name: "Temperatura",
+            color: "rgba(255,0,0,1)",
+            yValueFormatString: "Temperatura 00.00°C",
+            xValueType: "dateTime",
+            dataPoints: DataTemperatura
+        },
+        {
+            //axisYType: "secondary",
+            type: "spline",
+            showInLegend: true,
+            name: "Umidade",
+            color: "rgba(0,0,255,1)",
+            yValueFormatString: "Umidade #,##%",
+            xValueType: "dateTime",
+            dataPoints: DataUmidade
+        }],
+    });
+
+    function addData(json) {
+        let data = json["PCD_data"][PCD_NAME];
+
+        for (var i = 0; i < data.length; i++) {
+            let localtimestamp = (data[i].timestamp) * 1000
+            let datatimeUTC = ((localtimestamp) + ((10800) * 1000))
+            let HMS = new Date(localtimestamp).toISOString().slice(0, 19).replace('T', ' ');
+            DataTemperatura.push({
+                x: datatimeUTC,
+                y: data[i].Temperatura, label: HMS
+            });
+            DataUmidade.push({
+                x: datatimeUTC,
+                y: data[i].Umidade, label: HMS
+
+            });
+        }
+        RHTabsoluto.render();
+    }
+    //addData(dadosTeste())
+    $.getJSON("/rhtdata", addData);
+
+}
+
 function RHTUmidade() {
     var DataTemperatura = [];
     var DataUmidade = [];
@@ -200,7 +283,7 @@ function RHTUmidade() {
         legend: {
             fontSize: 15,
             fontFamily: "tamoha",
-            horizontalAlign: "center", // left, center ,right 
+            horizontalAlign: "center", // left, center ,right
             verticalAlign: "bottom",  // top, center, bottom
         },
         axisY: {
@@ -268,7 +351,7 @@ function RHTtemperatura() {
         legend: {
             fontSize: 15,
             fontFamily: "tamoha",
-            horizontalAlign: "center", // left, center ,right 
+            horizontalAlign: "center", // left, center ,right
             verticalAlign: "bottom",  // top, center, bottom
 
 
@@ -321,10 +404,4 @@ function RHTtemperatura() {
     //addData(dadosTeste())
     $.getJSON("/rhtdata", addData);
 }
-
-setInterval(function () {
-    RHTabsoluto()
-    RHTrelativo()
-    RHTUmidade()
-    RHTtemperatura()
-}, 60000)
+*/
