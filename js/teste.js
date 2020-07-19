24f75b6414821052b0b472f5d45ef9b460801d2b
Ricardo Paschoali <ricardo.paschoali@gmail.com>
2020-07-19 01:36:37 +0000

    .

--- js/teste.js
+++ js/teste.js
@@ -1,51 +1,57 @@
 
-window.onload = function () {
+const teste1 = []
+const teste2 = []
+const teste3 = []
+const teste4 = []
+teste = function (data) {
+    for (var i = 0; i < data.PCD_data["PCD-001"].length; i++) {
 
-    var dps = [{ x: 1, y: 10 }, { x: 2, y: 13 }, { x: 3, y: 18 }, { x: 4, y: 20 }, { x: 5, y: 17 }, { x: 6, y: 10 }, { x: 7, y: 13 }, { x: 8, y: 18 }, { x: 9, y: 20 }, { x: 10, y: 17 }];   //dataPoints. 
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        title: {
-            text: "Live Data"
-        },
-        axisX: {
-            title: "Axis X Title"
-        },
-        axisY: {
-            title: "Units"
-        },
-        data: [{
-            type: "line",
-            dataPoints: dps
-        }]
-    });
-
-    chart.render();
-    var xVal = dps.length + 1;
-    var yVal = 15;
-    var updateInterval = 1000;
-
-    var updateChart = function (json) {
-        let data = json["PCD_data"][PCD_NAME];
-
-        for (var i = 0; i < data.length; i++) {
-            let localtimestamp = (data[i].timestamp) * 1000
-            let datatimeUTC = ((localtimestamp) + ((10800) * 1000))
-            let HMS = new Date(localtimestamp).toISOString().slice(0, 19).replace('T', ' ');
-            DataTemperatura.push({
-                x: datatimeUTC,
-                y: data[i].Temperatura, label: HMS
-            });
-            DataUmidade.push({
-                x: datatimeUTC,
-                y: data[i].Umidade, label: HMS
-
-            });
-        }
-        RHTrelativo.render();
+        teste1.push([data.PCD_data["PCD-001"][i].timestamp * 1000, data.PCD_data["PCD-001"][i].Temperatura])
+        teste2.push([data.PCD_data["PCD-001"][i].timestamp * 1000, data.PCD_data["PCD-001"][i].Pressao / 100])
+        teste3.push([data.PCD_data["PCD-001"][i].timestamp * 1000, data.PCD_data["PCD-001"][i].Umidade])
+        teste4.push([data.PCD_data["PCD-001"][i].timestamp * 1000, data.PCD_data["PCD-001"][i].Altitude])
     }
-    $.getJSON("/rhtdata", updateChart);
-
-};
-
-setInterval(function () { updateChart() }, updateInterval);
+    console.log(teste1)
+    console.log(teste2)
+    console.log(teste3)
+    console.log(teste4)
 }
+Highcharts.getJSON('/rhtdata',
+    function (json) {
+        teste(json)
+        Highcharts.chart('container', {
+            chart: {
+                zoomType: 'x'
+            },
+            title: {
+                text: 'Plataforma de Coleta de Dados'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Exchange rate'
+                }
+            },
+            legend: {
+                enabled: true
+            },
+
+            series: [{
+                name: 'Temperatura',
+                data: teste1
+            }, {
+                name: 'Pressao',
+                data: teste2
+            }, {
+                name: 'Umidade',
+                data: teste3
+            }, {
+                name: 'Altitude',
+                data: teste4
+            }]
+        });
+    }
+)
+
